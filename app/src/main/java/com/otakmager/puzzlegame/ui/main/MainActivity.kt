63b7cff681f1ae2e33be73456d7233ed04208b21
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-05-10 20:33:15 +0700

    First commit

---
+++ app/src/main/java/com/otakmager/puzzlegame/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.otakmager.puzzlegame.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.otakmager.puzzlegame.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
