d5882954af51ec5fe983d784ab70694101b930a0
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2023-05-10 20:48:19 +0700

    Make splashScreen

--- app/src/main/java/com/otakmager/puzzlegame/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/otakmager/puzzlegame/ui/splashscreen/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.otakmager.puzzlegame.ui.splashscreen
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.otakmager.puzzlegame.databinding.ActivitySplashBinding
+import com.otakmager.puzzlegame.ui.main.MainActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Remove Action Bar
+        supportActionBar?.hide()
+
+        // Set timer to show and logic to move to the other activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
     }
 }
